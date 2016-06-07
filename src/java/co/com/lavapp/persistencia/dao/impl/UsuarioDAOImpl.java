a16f96371a0a6126c374a19bad065b2d912281d1
CristianRestrepo <cris940604@hotmail.com>
2016-06-07 12:33:13 -0500

    arreglos varios

--- src/java/co/com/lavapp/persistencia/dao/impl/UsuarioDAOImpl.java
+++ src/java/co/com/lavapp/persistencia/dao/impl/UsuarioDAOImpl.java
@@ -83,7 +83,7 @@ public class UsuarioDAOImpl implements UsuarioDAO {
         try {
             try {
                 String sql = "SELECT idusuario, nombre, telefono, idBarrios, idrol, idestado, email, contrasena, apellido, genero, movil, direccion, idciudad"
-                        + " FROM public.usuario where usuario.email = '" + usuario.getEmail() + "';";
+                        + " FROM public.usuario as usuario where usuario.email = '" + usuario.getEmail() + "';";
                 ResultSet rs = st.executeQuery(sql);
                 while (rs.next()) {
                     user = new Usuario_TO(rs.getInt(1),
@@ -132,7 +132,6 @@ public class UsuarioDAOImpl implements UsuarioDAO {
                 ResultSet rs = st.executeQuery(sql);
 
                 while (rs.next()) {
-
                     user = new Usuario_TO(rs.getInt(1), rs.getString(2), rs.getString(3), new Barrio_TO(rs.getInt(4)), new Rol_TO(rs.getInt(5)), new Estado_TO(rs.getInt(6)), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11), rs.getString(12), new Ciudad_TO(rs.getInt(13)));
                 }
             } catch (Exception e) {
